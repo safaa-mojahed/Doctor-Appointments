ca27945ad4bb7c6ec97dd7ba7b1939963c43d667
safaa_mujahed <safamojahed123@gmail.com>
2020-02-20 11:43:13 +0200

    connect to mongodb

---
+++ app/src/app.js
@@ -0,0 +1,11 @@
+const dbName = 'clinic'
+let db
+
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err)
+
+  // Storing a reference to the database so you can use it later
+  db = client.db(dbName)
+  console.log(`Connected MongoDB: ${url}`)
+  console.log(`Database: ${dbName}`)
+})
